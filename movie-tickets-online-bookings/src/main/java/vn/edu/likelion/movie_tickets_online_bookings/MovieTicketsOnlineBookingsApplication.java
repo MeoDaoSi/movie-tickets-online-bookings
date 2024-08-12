c114c5257802aad3539a9b1ac65098e075ae8fa0
TinhLe.210 <102019281+MeoDaoSi@users.noreply.github.com>
2024-08-12 11:36:42 +0700

    init project

---
+++ movie-tickets-online-bookings/src/main/java/vn/edu/likelion/movie_tickets_online_bookings/MovieTicketsOnlineBookingsApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.likelion.movie_tickets_online_bookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketsOnlineBookingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketsOnlineBookingsApplication.class, args);
+	}
+
+}
---
+++ movie-tickets-online-bookings/src/test/java/vn/edu/likelion/movie_tickets_online_bookings/MovieTicketsOnlineBookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.likelion.movie_tickets_online_bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketsOnlineBookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

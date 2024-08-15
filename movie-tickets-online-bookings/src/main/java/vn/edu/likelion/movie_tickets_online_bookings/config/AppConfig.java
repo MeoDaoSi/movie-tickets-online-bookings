23152e471186dac3c21b4507cbcff00907af040a
TinhLe.210 <102019281+MeoDaoSi@users.noreply.github.com>
2024-08-15 14:08:46 +0700

    feat create user

--- movie-tickets-online-bookings/src/main/java/vn/edu/likelion/movie_tickets_online_bookings/config/AppConfig.java
+++ movie-tickets-online-bookings/src/main/java/vn/edu/likelion/movie_tickets_online_bookings/config/AppConfig.java
@@ -3,6 +3,7 @@ package vn.edu.likelion.movie_tickets_online_bookings.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,11 +14,10 @@ public class AppConfig {
     @Bean
     public SecurityFilterChain springSecurityConfiguration(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
-                    authorizationManagerRequestMatcherRegistry
-                            .requestMatchers("/admin/**").hasRole("ADMIN");
-                });
-
+                .csrf(AbstractHttpConfigurer::disable) // Tắt CROSS ORIGIN
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                );
         return http.build();
     }
 
--- movie-tickets-online-bookings/src/main/java/vn/edu/likelion/movie_tickets_online_bookings/controller/UserController.java
+++ movie-tickets-online-bookings/src/main/java/vn/edu/likelion/movie_tickets_online_bookings/controller/UserController.java
@@ -1,10 +1,27 @@
 package vn.edu.likelion.movie_tickets_online_bookings.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.edu.likelion.movie_tickets_online_bookings.dto.request.UserRequest;
+import vn.edu.likelion.movie_tickets_online_bookings.dto.response.UserResponse;
+import vn.edu.likelion.movie_tickets_online_bookings.exception.UserException;
+import vn.edu.likelion.movie_tickets_online_bookings.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    private ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest userRequest) {
+        UserResponse userResponse = userService.create(userRequest);
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
+    }
 }

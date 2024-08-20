b982a8ded897d079b259a1d1d0a167253548abd7
Bui Quoc Thinh <quocthinh.bui.6339@gmail.com>
2024-08-20 14:27:29 +0700

    Perceval Aug_20 commit 1

--- target/generated-sources/annotations/vn/edu/likelion/movie_tickets_online_bookings/mapper/SeatMapperImpl.java
+++ target/generated-sources/annotations/vn/edu/likelion/movie_tickets_online_bookings/mapper/SeatMapperImpl.java
@@ -9,7 +9,7 @@ import vn.edu.likelion.movie_tickets_online_bookings.entity.SeatEntity;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-19T09:30:40+0700",
+    date = "2024-08-20T14:07:11+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
 )
 @Component
